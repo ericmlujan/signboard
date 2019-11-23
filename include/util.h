612fdadb54850bfb53657ddb6df6c004615b2a13
Eric Lujan <lujan@mit.edu>
2019-11-23 13:26:32 -0800

    Clean up networking utilities

--- include/util.h
+++ include/util.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include "rapidxml.hpp"
-
 #include <cctype>
 #include <cstring>
 #include <memory>
 #include <string>
 #include <vector>
 
+#include "curl/curl.h"
+
 namespace signboard {
-// A simple dynamically allocated buffer that contains bytes of data // Note that this data structure will grow monotonically in size until
+// A simple dynamically allocated buffer that contains bytes of data
+// Note that this data structure will grow monotonically in size until
 // destructed
 struct MemoryBuffer {
   char *data;
@@ -38,6 +39,44 @@ struct MemoryBuffer {
   std::string toString() { return std::string(data); }
 
   void append(const std::string &str) { this->append(str.c_str(), str.size()); }
+};
+
+inline size_t HandleHTTPResponse(char *bytes, size_t size, size_t nmemb,
+                                 void *out) {
+  MemoryBuffer *buf = (MemoryBuffer *)out;
+  size_t adjustedSize = size * nmemb;
+  if (!buf) {
+    return 0;
+  }
+
+  buf->append(bytes, adjustedSize);
+  return adjustedSize;
+}
+
+inline size_t GetHTTP(const std::string &url, std::string *out) {
+  auto handle = curl_easy_init();
+  MemoryBuffer buf;
+  size_t httpResponse = 0;
+
+  struct curl_slist *list;
+  list = curl_slist_append(NULL, "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0");
+
+  curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, HandleHTTPResponse);
+  curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&buf);
+  curl_easy_setopt(handle, CURLOPT_HTTPHEADER, list);
+
+  // Perform the HTTP request
+  CURLcode result = curl_easy_perform(handle);
+  if (result != CURLE_OK) {
+    fprintf(stderr, "error in performing HTTP request: %s\n",
+            curl_easy_strerror(result));
+  } else {
+    *out = buf.toString();
+    curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &httpResponse);
+  }
+  curl_easy_cleanup(handle);
+  return httpResponse;
 };
 
 // Find the index of the first occurence of a substring within a string if one
@@ -74,12 +113,20 @@ inline void RemoveSubstring(const std::string &substr, std::string *string) {
   }
 }
 
-inline std::string ToUpper(const std::string& str) {
+inline std::string ToUpper(const std::string &str) {
   std::string res;
   for (const auto &ch : str) {
     res += std::toupper(ch);
   }
   return res;
+}
+
+template<typename ...Args>
+std::string Format(const std::string& formatString, Args... args) {
+  size_t size = snprintf(nullptr, 0, formatString.c_str(), args...) + 1;
+  std::unique_ptr<char[]> buf(new char[size]);
+  snprintf(buf.get(), size, formatString.c_str(), args...);
+  return std::string(buf.get(), buf.get() + size - 1);
 }
 
 } // namespace signboard
--- src/networking.cpp
+++
@@ -1,5 +0,0 @@
-#include "networking.h"
-
-namespace signboard {
-
-}
