f2466755fd4209ff1d00ea6d8626f53652b963c4
Eric Lujan <lujan@mit.edu>
2019-11-23 13:25:12 -0800

    Add text helpers

--- include/graphics_util.h
+++ include/graphics_util.h
@@ -5,6 +5,9 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
+#include <memory>
+#include <string>
 
 namespace signboard {
 // Defines how a column should be aligned within a line
@@ -13,25 +16,37 @@ enum ColumnAlignment { LEFT = 0, CENTER = 1, RIGHT = 2 };
 struct Column {
   std::string text = "";
   ColumnAlignment alignment = ColumnAlignment::LEFT;
-  void operator=(const std::string &text);
+  void operator=(const std::string &rhs) { text = rhs; }
 };
 
 // A line of text consisting of three columns of text
 // TODO: should the line also set its own font?
 struct TextLine {
-  TextLine() {
+  TextLine(const std::string &l, const std::string &c, const std::string &r) {
     cols.resize(3);
+    cols[0].alignment = ColumnAlignment::LEFT;
+    cols[0] = l;
     cols[1].alignment = ColumnAlignment::CENTER;
+    cols[1] = c;
     cols[2].alignment = ColumnAlignment::RIGHT;
+    cols[2] = r;
   }
 
+  template <typename T> TextLine(const T &text) : TextLine(text, "", "") {}
+
+  TextLine() : TextLine("", "", "") {}
+
+  void operator=(const std::string &text) { cols[0] = text; }
+
   std::vector<Column> cols;
-  rgb_matrix::Color color{255, 255, 255};
+  rgb_matrix::Color color{255, 200, 0};
 };
 
 void FormatText(const std::vector<TextLine> &lines,
-                       const rgb_matrix::Font &font,
-                       rgb_matrix::FrameCanvas *canvas);
+                const rgb_matrix::Font &font, rgb_matrix::Canvas *canvas);
 void FormatLine(const TextLine &line, const rgb_matrix::Font &font,
-                       const size_t baselineY, rgb_matrix::FrameCanvas *canvas);
+                const size_t baselineY, rgb_matrix::Canvas *canvas);
+
+std::vector<TextLine> WrapText(const std::string &text,
+                               const rgb_matrix::Font &font, size_t lineWidth);
 } // namespace signboard
--- src/graphics_util.cpp
+++ src/graphics_util.cpp
@@ -1,11 +1,9 @@
 #include "graphics_util.h"
+#include "lib/utf8-internal.h"
 
 namespace signboard {
-void Column::operator=(const std::string &rhs) { text = rhs; }
-
 void FormatText(const std::vector<TextLine> &lines,
-                const rgb_matrix::Font &font, rgb_matrix::FrameCanvas *canvas) {
-  canvas->Clear();
+                const rgb_matrix::Font &font, rgb_matrix::Canvas *canvas) {
   size_t baselineY = std::max(0, font.baseline());
   for (const auto &line : lines) {
     FormatLine(line, font, baselineY, canvas);
@@ -15,7 +13,7 @@ void FormatText(const std::vector<TextLine> &lines,
 
 // Format a line on the canvas, with the baseline at the y value of baselineY
 void FormatLine(const TextLine &line, const rgb_matrix::Font &font,
-                const size_t baselineY, rgb_matrix::FrameCanvas *canvas) {
+                const size_t baselineY, rgb_matrix::Canvas *canvas) {
   // TODO: bounds checking for columns running into one another, right now the
   // text will just be on top of itself
   if (!canvas) {
@@ -37,8 +35,8 @@ void FormatLine(const TextLine &line, const rgb_matrix::Font &font,
     case ColumnAlignment::CENTER:
       // Find the centre of the canvas, then subtract off half the width of the
       // column
-      x =
-        std::max<size_t>(0, std::floor<size_t>(canvasWidth / 2 - colWidth / 2));
+      x = std::max<size_t>(0,
+                           std::floor<size_t>(canvasWidth / 2 - colWidth / 2));
       rgb_matrix::DrawText(canvas, font, x, baselineY, line.color, nullptr,
                            col.text.c_str(), 0);
       break;
@@ -51,4 +49,40 @@ void FormatLine(const TextLine &line, const rgb_matrix::Font &font,
     }
   }
 }
+
+std::vector<TextLine> WrapText(const std::string &text,
+                               const rgb_matrix::Font &font, size_t lineWidth) {
+  std::vector<TextLine> lines;
+  // Iterate through each character of the string
+  size_t currentWidth = 0;
+  size_t lineStartIdx = 0;
+  // Read one past the size of the string to capture the terminating null byte
+  for (size_t i = 0; i < text.size() + 1; ++i) {
+    // Encountered a newline; this automatically wraps!
+    if (text[i] == '\n' || text[i] == '\0') {
+      lines.emplace_back(text.substr(lineStartIdx, i - lineStartIdx));
+      currentWidth = 0;
+      lineStartIdx = i + 1;
+      continue;
+    }
+
+    auto iter = text.begin() + i;
+    uint32_t cp = utf8_next_codepoint(iter);
+    currentWidth += font.CharacterWidth(cp);
+
+    // Read past the end of the screen. Backtrack to the last known space
+    if (currentWidth > lineWidth) {
+      for (; i > 0; --i) {
+        if (text[i] == ' ' || text[i] == '\n' || text[i] == '\t') {
+          lines.emplace_back(text.substr(lineStartIdx, (int)i - lineStartIdx));
+          lineStartIdx = i + 1;
+          i++;
+          currentWidth = 0;
+          break;
+        }
+      }
+    }
+  }
+  return lines;
+};
 } // namespace signboard
